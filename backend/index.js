43e31355125a5e084ae7ba73156871c7dd440905
harshitcompcode <harshit.k.61120@gmail.com>
2023-09-08 12:21:42 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": "604ce248-79fc-4ff8-a362-d335f7fc5c78" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(3001);
